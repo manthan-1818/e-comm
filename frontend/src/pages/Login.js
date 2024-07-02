6cf5f0799afbb58f4ab014312b458b329f7dbf40
manthan-1818 <manthanvaghela1818@gmail.com>
2024-07-02 13:17:41 +0530

    script

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,11 +4,9 @@ import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import axiosInstance from "../utils/services/axios";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import { loginSuccess, loginFailure } from "../redux/slice/authSlice";
 import { set_token } from "../utils/services/userservices";
 import "../css/Login.css";
-
 
 const Login = () => {
   const navigate = useNavigate();
@@ -32,7 +30,7 @@ const Login = () => {
         set_token(accessToken);
         localStorage.setItem("refreshToken", refreshToken);
         dispatch(loginSuccess({ user, accessToken }));
-         console.log("User role:", user.role);
+        console.log("User role:", user.role);
         navigate("/");
       } else {
         console.error("Login failed:", message);
@@ -48,42 +46,49 @@ const Login = () => {
     <div>
       <Navbar />
       <div className="login-container">
-  <div className="login-form">
-    <h2>Login</h2>
-    <form onSubmit={handleSubmit(handleLogin)}>
-      <div className="input-group">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", { required: true })}
-          placeholder="Enter your email"
-        />
-        {errors.email && <p className="error">Email is required</p>}
+        <div className="login-form">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                {...register("email", { required: "Email is required" })}
+                placeholder="Enter your email"
+              />
+              {errors.email && <p className="error">{errors.email.message}</p>}
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                {...register("password", {
+                  required: "Password is required",
+                  pattern: {
+                    value: /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                    message:
+                      "Please enter a valid password. Must be at least 8 characters long.",
+                  },
+                })}
+                placeholder="Enter your password"
+              />
+              {errors.password && (
+                <p className="error">{errors.password.message}</p>
+              )}
+            </div>
+            <div className="btn-group">
+              <button type="submit" className="login-btn">
+                Login
+              </button>
+            </div>
+          </form>
+          <p className="mt-2">
+            Don't have an account? <a href="/signup">Sign Up</a>
+          </p>
+        </div>
       </div>
-      <div className="input-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          {...register("password", { required: true })}
-          placeholder="Enter your password"
-        />
-        {errors.password && <p className="error">Password is required</p>}
-      </div>
-      <div className="btn-group">
-        <button type="submit" className="login-btn">
-          Login
-        </button>
-      </div>
-    </form>
-    <p className="mt-2">
-      Don't have an account? <a href="/signup">Sign Up</a>
-    </p>
-  </div>
-</div>
-
-      {/* <Footer /> */}
     </div>
   );
 };
